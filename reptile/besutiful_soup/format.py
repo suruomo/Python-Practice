0f50db5d13e8279a76c8956eb5e6bd3d07abd798
suruomo <www.suruomo@hotmail.com>
2020-03-22 16:10:52 +0800

    移除神经网络和爬虫

--- reptile/besutiful_soup/format.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-kv={'user-agent':'Mozilla/5.0'}
-url="http://python123.io/ws/demo.html"
-r=requests.get(url,headers=kv)
-demo=r.text
-soup=BeautifulSoup(demo,"html.parser")
-print(soup.a.prettify())
--- reptile/requests/360.py
+++
@@ -1,5 +0,0 @@
-import requests
-kv={'q':'Python'}
-r=requests.get("http://www.so.com",params=kv)
-print(r.status_code)
-print(r.request.url)
